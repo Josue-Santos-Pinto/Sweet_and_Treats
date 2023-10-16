3826e706f1db5d3cb0528564095f1133b4542cca
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-10-16 18:47:31 -0300

    request location permission apply

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,11 +1,30 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import { PermissionsAndroid } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { Container } from './styles';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { WEB_CLIENT_ID } from '../../helpers';
+import Geolocation from 'react-native-geolocation-service';
+import { request, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 export default function Location() {
-  return (
-    <Container>
-      <Text style={{ color: '#000' }}>Como chegar</Text>
-    </Container>
-  );
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  const destinationCoords = {
+    latitude: -22.828870998276898,
+    longitude: -42.091118816140714,
+  };
+
+  const requestLocationPermission = async () => {
+    request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then((result) => {
+      console.log(result);
+    });
+  };
+
+  useEffect(() => {
+    // Obter a localização atual do usuário
+    requestLocationPermission();
+  }, []);
+
+  return <Container></Container>;
 }
