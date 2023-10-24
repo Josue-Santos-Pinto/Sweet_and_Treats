a2996d27d3deac1bbb1febf4762b89df0c4acdc0
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-10-24 20:56:37 -0300

    OneSignal apply

--- src/screens/Notifications/index.tsx
+++ src/screens/Notifications/index.tsx
@@ -1,8 +1,21 @@
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from './styles';
+import { OneSignal } from 'react-native-onesignal';
+import { NOTIFICATIONS_ID } from '../../helpers';
 
 export default function Notifications() {
+  useEffect(() => {
+    OneSignal.initialize(NOTIFICATIONS_ID);
+    OneSignal.Notifications.requestPermission(true);
+    OneSignal.Notifications.addEventListener('click', onOpened);
+    return () => OneSignal.Notifications.removeEventListener('click', onOpened);
+  }, []);
+
+  const onOpened = (results: any) => {
+    console.log('Mensagem: ', results.notification.payload.body);
+  };
+
   return (
     <Container>
       <Text style={{ color: '#000' }}>Notificações</Text>
